618a3b54bf0f6641c98e6d92c8c821151f6b96ae
Natthawut <s5935512064@phuket.psu.ac.th>
2020-05-08 12:23:03 +0700

    upload source code

--- src/pages/ShowAct.vue
+++ src/pages/ShowAct.vue
@@ -1,23 +1,44 @@
 <template>
-  <div class="container">
-    <h2>Heading</h2>
-    <table class="table table-bordered" id="tbresult">
-      <tr>
-        <th>Name</th>
-        <th>Province</th>
-        <th>Age</th>
-        <th>Process</th>
-      </tr>
-    </table>
+  <div class="q-pa-md">
+
+    <div class="row q-col-gutter-x-xs q-col-gutter-y-lg">
+      <div class="col-4" v-for="n in 5" :key="`none-${n}`">
+        
+        <div class="my-content">
+           <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Overline</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">กิจกรรมรับน้อง</div>
+          <div class="text-caption text-grey">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-5 flex flex-center">
+          <q-img
+            class="rounded-borders"
+            src="https://sv1.picz.in.th/images/2020/05/08/UrF5BE.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat>
+          7:30PM
+        </q-btn>
+        <q-btn flat color="primary">
+          Online Register
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+    
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
-
-<script>
-  export default {
-    
-  }
-</script>
-
-<style scoped>
-
-</style>
